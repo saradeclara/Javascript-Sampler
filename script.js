9dcadd91ef5c6552e9d9f5d640f74f2ab246c537
Sara De Clara <saradeclara@gmail.com>
2019-03-29 17:36:50 +0000

    Audio files added to new audio directory. Audio playback added to JS functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('keydown', function(e) {
     if (e.keyCode == 65) {
         let activeButton = document.getElementById('a');
         activeButton.classList.add("active");
+        document.getElementById("kick").play();
     }
 })
 
@@ -9,5 +10,7 @@ document.addEventListener('keyup', function(e) {
     if (e.keyCode == 65) {
         let activeButton = document.getElementById('a');
         activeButton.classList.remove("active");
+        document.getElementById("kick").pause();
+        document.getElementById("kick").currentTime = 0;
     }
 })
