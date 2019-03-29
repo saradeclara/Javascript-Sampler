780252a1680fef2e21461e63a0fb0bdbe2c95335
Sara De Clara <saradeclara@gmail.com>
2019-03-29 17:49:53 +0000

    Snare button completed. New audio file added (percussion)

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ document.addEventListener('keyup', function(e) {
         document.getElementById("kick").currentTime = 0;
     }
 })
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+        let activeButton = document.getElementById('s');
+        activeButton.classList.add("active");
+        document.getElementById("snare").play();
+    }
+})
+
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 83) {
+        let activeButton = document.getElementById('s');
+        activeButton.classList.remove("active");
+        document.getElementById("snare").pause();
+        document.getElementById("snare").currentTime = 0;
+    }
+})
