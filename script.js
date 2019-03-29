d748322b4cf3472bf125ce5d358abfdc3a10c1f9
Sara De Clara <saradeclara@gmail.com>
2019-03-29 18:06:51 +0000

    All IDs added to key elements in index.html

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     if (e.keyCode == 65) {
         let activeButton = document.getElementById('a');
         activeButton.classList.add("active");
         document.getElementById("kick").play();
+    } else if (e.keyCode == 83) {
+        let activeButton = document.getElementById('s');
+        activeButton.classList.add("active");
+        document.getElementById("snare").play();
+    } else if (e.keyCode == 68) {
+        let activeButton = document.getElementById('d');
+        activeButton.classList.add("active");
+        document.getElementById("close-hat").play();
     }
 })
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     if (e.keyCode == 65) {
         let activeButton = document.getElementById('a');
         activeButton.classList.remove("active");
         document.getElementById("kick").pause();
         document.getElementById("kick").currentTime = 0;
-    }
-})
-
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 83) {
-        let activeButton = document.getElementById('s');
-        activeButton.classList.add("active");
-        document.getElementById("snare").play();
-    }
-})
-
-document.addEventListener('keyup', function(e) {
-    if (e.keyCode == 83) {
+    } else if (e.keyCode == 83) {
         let activeButton = document.getElementById('s');
         activeButton.classList.remove("active");
         document.getElementById("snare").pause();
         document.getElementById("snare").currentTime = 0;
+    } if (e.keyCode == 68) {
+        let activeButton = document.getElementById('d');
+        activeButton.classList.remove("active");
+        document.getElementById("close-hat").pause();
+        document.getElementById("close-hat").currentTime = 0;
     }
 })
